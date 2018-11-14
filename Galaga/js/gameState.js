8a796a7d956e48c12411f8da6cf948dd8541e1c6
ENTI\sergisanchezsolares <seychey14@hotmail.com>
2018-11-14 12:41:39 +0100

    Starting with Normal Stage

--- Galaga/js/gameState.js
+++ Galaga/js/gameState.js
@@ -11,11 +11,18 @@ galaga.gameState = {
         this.load.image('star', 'assets/img/spr_star.png');
         this.load.image('bullet', 'assets/img/spr_bullet_0.png');
         this.load.spritesheet('enemyMedium','assets/img/enemy-medium.png', 32, 16);
-        this.load.spritesheet('nave', 'assets/img/spr_player.png', 16, 24);
-        
+        this.load.spritesheet('nave', 'assets/img/player.png', 22, 24);
+        this.load.image('insignia1', 'assets/img/insignia_1.png' );
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
         this.bulletSpeed = -100; 
         this.speed = 2;
+        
+        this.score=0;
+        this.highScore= 20000;
+        this.numberStage=1;
+        this.lifes = 3;
+        
+        this.isPaused = false;
         //this.enemyMediumSpeed=20;
         
         
@@ -24,8 +31,7 @@ galaga.gameState = {
     create:function(){
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
        
-        
-       
+      
         
        
         /*this.bg1 =  this.game.add.tileSprite(0,0,128,256,'bg1');
@@ -33,13 +39,11 @@ galaga.gameState = {
         this.bg1.scale.setTo(2);
         this.bg2.scale.setTo(2);*/
         this.initStars();
-         
+        this.initHud(); 
         
         this.nave = this.game.add.sprite(this.game.world.centerX,this.game.world.height - 50,'nave');
-        this.nave.animations.add('stand',[0,1],10,true);
-        this.nave.animations.add('left',[2,3],10,true);
-        this.nave.animations.add('right',[4,5],10,true);
-        this.nave.scale.setTo(2);
+      
+       // this.nave.scale.setTo(2);
         this.nave.anchor.setTo(.5);
         this.cursores = this.input.keyboard.createCursorKeys();
         
@@ -49,34 +53,102 @@ galaga.gameState = {
       
         this.game.physics.arcade.enable(this.nave);
         this.nave.body.collideWorldBounds = true;
+        
+        this.pKey = this.game.input.keyboard.addKey(Phaser.Keyboard.P);
+        
        // this.loadBullets();
        // this.loadEnemyMedium();
         
       
     },
     
-    initStars:function(){
-         this.stars = this.add.group();
-       
+    initHud:function(){
+        this.stageOffset = 10;
+        for(var i=1; i<=this.numberStage;i++){
+        this.insigniaStage = this.game.add.sprite(gameOptions.gameWidth - (i*this.stageOffset), gameOptions.gameHeight - 20,'insignia1');
+        }
         
+        this.lifesOffset = 24;
+        for(var i=1; i<this.lifes;i++){
+        this.insigniaStage = this.game.add.sprite(i*this.lifesOffset-20, gameOptions.gameHeight - 28,'nave');
+        }
+        
+        this.topStyle = { font: "15px arcade", fill: "#FF0000",};
+        this.scoreStyle = { font: "15px arcade", fill: "#FFFFFF", align: "center",};
+        this.playerText = this.game.add.text(0,0, "1UP", this.topStyle); 
+        this.playerScore = this.game.add.text(15,15, this.score, this.scoreStyle); 
+        
+        this.topText = this.game.add.text(gameOptions.gameWidth/2 - 30,0, "HIGHSCORE", this.topStyle); 
+        this.playerScore = this.game.add.text(gameOptions.gameWidth/2,15, this.highScore, this.scoreStyle); 
+        
+        
+
+    },
+    
+    initStars:function(){
+        // this.stars = this.add.group();
+       
+         this.stars = [];
+         
          for(var i=0; i<=100; i++)
          {
+             this.isVisible = Math.floor(Math.random() * 2);
+             this.active = false;
+         
+             if(this.isVisible == 0){
+                 this.active = false;
+               
+             }
+             else{
+                 this.active = true;
+                
+             }
+             
+                          
+             
+            this.star = new galaga.starBehavior(this.game,Math.random() * gameOptions.gameWidth,Math.random() * gameOptions.gameHeight,this.active);
+             
+           
+             if(this.isVisible == 0){
+                 
+                 this.star.visible = false;
+             }
+             else{
+               
+                 this.star.visible = true;
+             }
             
-            this.stars.create(Math.random() * gameOptions.gameWidth,Math.random() * gameOptions.gameHeight, 'star');
+             
+           this.stars.push( this.game.add.existing(this.star));
+             
+            // this.game.add.existing(this.star);
+            
             
          } 
-        this.starFlareTimer = this.game.time.events.loop(Phaser.Timer.SECOND*2,this.flareStar,this);
+        
+       this.starFlareTimer = this.game.time.events.loop(Phaser.Timer.SECOND/2,this.flareStar,this);
         
     },
     
-    flareStar:function( ){
-     
-        
-         /*for(var i=0; i<100; i+=2)
-             {
-                 this.stars.remove(item);
-             }*/
-    },
+   flareStar:function(){
+       for(var i=0; i<=100; i++)
+         {
+             
+             if(this.stars[i].active)
+                 {
+                     this.stars[i].active = false;
+                     
+                 this.stars[i].visible = false;
+                 }
+             else{
+                     this.stars[i].active = true;   
+                 
+                 this.stars[i].visible = true;
+             }
+             
+              
+         }
+   },
     
     loadBullets:function(){
         this.bullets = this.add.group();
@@ -122,6 +194,7 @@ galaga.gameState = {
     
     update:function(){
         
+       
         /*this.bg1.tilePosition.y += .95;
         this.bg2.tilePosition.y += .50;*/
         
@@ -135,9 +208,28 @@ galaga.gameState = {
              this.nave.body.velocity.x -= this.speed;
         }else{
             this.nave.animations.play('stand');
+            
+             this.nave.body.velocity.x =0;
+        }
+        
+        if(this.pKey.isDown){
+            console.log("·hola"); 
+            if(!this.isPaused)
+            {
+                this.pauseText = this.game.add.text(gameOptions.gameWidth/2, gameOptions.gameHeight/2, 'Paused', this.scoreStyle);
+
+                this.game.paused = true; 
+                
+            }
+            else{
+                this.pauseText.destroy();
+                this.game.paused = false;
+            }
         }
         
         
         
-    }
+    },
+     
+    
 }
--- Galaga/js/starBehavior.js
+++ Galaga/js/starBehavior.js
@@ -1,14 +1,17 @@
 var galaga = galaga || {};
 
  
-galaga.starBehavior = function(game,x,y){
-    Phaser.Sprite.call(this,game,x,y,'spr_star');
+galaga.starBehavior = function(game,x,y,active){
+    Phaser.Sprite.call(this,game,x,y,'star');
     this.anchor.setTo(.5);
     this.checkWorldBounds = true;
     this.outOfBoundsKill = true;
-    
+    this.active = active;
+ 
 };
 
 galaga.starBehavior.prototype = Object.create(Phaser.Sprite.prototype);
 
 galaga.starBehavior.prototype.constructor = galaga.starBehavior;
+
+ 
